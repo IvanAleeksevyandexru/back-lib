96926451637b2629dc3eaa03591853fbb4b30fcf
Grigoriy Loparev <gloparev@it-one.ru>
2022-02-08 11:37:46 +0300

    EPGUCORE-74909: Рефакторинг QuestionScrComponent + обновление теста

--- pgu-common/pgu-fs-common/src/main/java/ru/gosuslugi/pgu/fs/common/component/input/QuestionScrComponent.java
+++ pgu-common/pgu-fs-common/src/main/java/ru/gosuslugi/pgu/fs/common/component/input/QuestionScrComponent.java
@@ -15,7 +15,6 @@ import ru.gosuslugi.pgu.components.descriptor.placeholder.PlaceholderContext;
 import ru.gosuslugi.pgu.components.descriptor.types.Action;
 import ru.gosuslugi.pgu.dto.descriptor.types.ComponentType;
 import ru.gosuslugi.pgu.fs.common.component.AbstractComponent;
-import ru.gosuslugi.pgu.fs.common.service.ComponentReferenceService;
 
 import java.util.*;
 
@@ -24,94 +23,68 @@ import java.util.*;
 @RequiredArgsConstructor
 public class QuestionScrComponent extends AbstractComponent<String> {
 
-    private final ComponentReferenceService componentReferenceService;
-
     @Override
     public ComponentType getType() {
         return ComponentType.QuestionScr;
     }
 
     /**
-     * Выполняет тиражирование action-ов для цикличных ответов
-     * @param component компонент
+     * Выполняет тиражирование action-ов для цикличных ответов.
+     * Может перевычислить и перезаписать значения атрибутов actions и answers у компонента.
+     *
+     * @param component   компонент
      * @param scenarioDto DTO
-     * @return предустановленное значение для компонента
      * @see Action
      */
     @Override
     protected void preProcess(FieldComponent component, ScenarioDto scenarioDto) {
         FieldComponentAttrsFactory attrsFactory = new FieldComponentAttrsFactory(component);
-        var actionList = (List<Map<String, Object>>)Optional.ofNullable(component.getAttrs()).orElse(Collections.emptyMap())
-                .getOrDefault(FieldComponentUtil.ACTIONS_ATTR_KEY, new ArrayList<>());
-
         PlaceholderContext context = componentReferenceService.buildPlaceholderContext(attrsFactory, component, scenarioDto);
+        replicateActionsOrAnswers(component, scenarioDto, FieldComponentUtil.ACTIONS_ATTR_KEY, attrsFactory, context);
+        replicateActionsOrAnswers(component, scenarioDto, FieldComponentUtil.ANSWERS_ATTR_KEY, attrsFactory, context);
+    }
 
-        for(int actionIndex = 0; actionIndex < actionList.size(); actionIndex++) {
-            var actionMap = actionList.get(actionIndex);
+    private void replicateActionsOrAnswers(FieldComponent component, ScenarioDto scenarioDto, String attributeName, FieldComponentAttrsFactory attrsFactory, PlaceholderContext context) {
+        List<Map<String, Object>> buf = new ArrayList<>();
+
+        @SuppressWarnings("unchecked")
+        var replicationItemList = (List<Map<String, Object>>) Optional.ofNullable(component.getAttrs())
+                .orElse(Collections.emptyMap())
+                .getOrDefault(attributeName, new ArrayList<>());
+
+        for (Map<String, Object> actionMap : replicationItemList) {
             Action action = attrsFactory.createAction(actionMap);
             CycledAttrs cycledAttrs = action.getAttrs();
 
-            boolean isCycled = !StringUtils.isEmpty(cycledAttrs.getCycledAnswerId());
-            if(isCycled) {
-                // Удаляем из action-ов action, который нужно растиражировать на основе цикличного блока
-                Action cycledAction = attrsFactory.createAction(actionList.remove(actionIndex--));
-
-                // Цикличный блок с ответами
-                var cycledApplicantAnswersOptional = Optional.ofNullable(scenarioDto.getCycledApplicantAnswers());
-                var cycledApplicantAnswerItems = cycledApplicantAnswersOptional.map(answers -> answers.getAnswerOrDefault(cycledAttrs.getCycledAnswerId(), null)).map(CycledApplicantAnswer::getItems).orElse(Collections.emptyList());
-
-                // Добавление новых action-ов на основе ответов в цикличном блоке
-                for(CycledApplicantAnswerItem cycledApplicantAnswerItem : cycledApplicantAnswerItems) {
-                    var answersMap = cycledApplicantAnswerItem.getItemAnswers();
-
-                    // Добавление нового action-а
-                    Map<String, Object> actionToAdd = new LinkedHashMap<>();
-                    actionToAdd.put("action", cycledAction.getAction());
-                    actionToAdd.put("label", componentReferenceService.getValueByContext(cycledAction.getLabel(), context, answersMap));
-                    actionToAdd.put("value", cycledApplicantAnswerItem.getId());
-                    actionToAdd.put("type", cycledAction.getType());
-                    actionList.add(++actionIndex, actionToAdd);
+            if (!StringUtils.isEmpty(cycledAttrs.getCycledAnswerId())) {
+                var cycledApplicantAnswerItems = getCycledApplicantAnswerItemsByCycledAnswerId(scenarioDto, cycledAttrs.getCycledAnswerId());
+                for (CycledApplicantAnswerItem cycledApplicantAnswerItem : cycledApplicantAnswerItems) {
+                    buf.add(createActions(action, context, cycledApplicantAnswerItem));
                 }
             } else {
                 // Обновляем лейбл action-а плейсхолдерами
-                actionMap.put("label", componentReferenceService.getValueByContext((String)actionMap.get("label"), context, scenarioDto.getApplicantAnswers(), scenarioDto.getCurrentValue()));
+                actionMap.put("label", componentReferenceService.getValueByContext((String) actionMap.get("label"), context, scenarioDto.getApplicantAnswers(), scenarioDto.getCurrentValue()));
+                buf.add(actionMap);
             }
         }
-
-        // TODO: нужно вынести в отдельный метод. DRY
-        var answersList = (List<Map<String, Object>>)Optional.ofNullable(component.getAttrs()).orElse(Collections.emptyMap())
-                .getOrDefault(FieldComponentUtil.ANSWERS_ATTR_KEY, new ArrayList<>());
-
-        for(int answerIndex = 0; answerIndex < answersList.size(); answerIndex++) {
-            var answerMap = answersList.get(answerIndex);
-            Action answerItem = attrsFactory.createAction(answerMap);
-            CycledAttrs cycledAttrs = answerItem.getAttrs();
-
-            boolean isCycled = !StringUtils.isEmpty(cycledAttrs.getCycledAnswerId());
-            if(isCycled) {
-                // Удаляем из action-ов action, который нужно растиражировать на основе цикличного блока
-                Action cycledAnswer = attrsFactory.createAction(answersList.remove(answerIndex--));
-
-                // Цикличный блок с ответами
-                var cycledApplicantAnswersOptional = Optional.ofNullable(scenarioDto.getCycledApplicantAnswers());
-                var cycledApplicantAnswerItems = cycledApplicantAnswersOptional.map(answers -> answers.getAnswerOrDefault(cycledAttrs.getCycledAnswerId(), null)).map(CycledApplicantAnswer::getItems).orElse(Collections.emptyList());
-
-                // Добавление новых action-ов на основе ответов в цикличном блоке
-                for(CycledApplicantAnswerItem cycledApplicantAnswerItem : cycledApplicantAnswerItems) {
-                    var answersMap = cycledApplicantAnswerItem.getItemAnswers();
-
-                    // Добавление нового action-а
-                    Map<String, Object> actionToAdd = new LinkedHashMap<>();
-                    actionToAdd.put("action", cycledAnswer.getAction());
-                    actionToAdd.put("label", componentReferenceService.getValueByContext(cycledAnswer.getLabel(), context, answersMap));
-                    actionToAdd.put("value", cycledApplicantAnswerItem.getId());
-                    actionToAdd.put("type", cycledAnswer.getType());
-                    answersList.add(++answerIndex, actionToAdd);
-                }
-            } else {
-                // Обновляем лейбл action-а плейсхолдерами
-                answerMap.put("label", componentReferenceService.getValueByContext((String)answerMap.get("label"), context, scenarioDto.getApplicantAnswers(), scenarioDto.getCurrentValue()));
-            }
-        }
+        component.getAttrs().put(attributeName, buf);
     }
+
+    private List<CycledApplicantAnswerItem> getCycledApplicantAnswerItemsByCycledAnswerId(ScenarioDto scenarioDto, String cycledAnswerId) {
+        var cycledApplicantAnswersOptional = Optional.ofNullable(scenarioDto.getCycledApplicantAnswers());
+        return cycledApplicantAnswersOptional.map(answers -> answers.getAnswerOrDefault(cycledAnswerId, null))
+                .map(CycledApplicantAnswer::getItems)
+                .orElse(Collections.emptyList());
+    }
+
+    private Map<String, Object> createActions(Action cycledAction, PlaceholderContext context, CycledApplicantAnswerItem cycledApplicantAnswerItem) {
+        var answersMap = cycledApplicantAnswerItem.getItemAnswers();
+        Map<String, Object> actionToAdd = new LinkedHashMap<>();
+        actionToAdd.put("action", cycledAction.getAction());
+        actionToAdd.put("label", componentReferenceService.getValueByContext(cycledAction.getLabel(), context, answersMap));
+        actionToAdd.put("value", cycledApplicantAnswerItem.getId());
+        actionToAdd.put("type", cycledAction.getType());
+        return actionToAdd;
+    }
+
 }
